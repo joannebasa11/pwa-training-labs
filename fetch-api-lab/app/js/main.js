2ea8f7699f4bd391d638e6d8c4ff41fbaab9c37b
Joanne G. Basa <joannegbasa@gmail.com>
2017-10-31 12:53:32 +0000

    completed fetch-api lab

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -24,56 +24,104 @@ var app = (function() {
     console.log('Looks like there was a problem: \n', error);
   }
 
-  // TODO 2.1a
+  if (!('fetch' in window)) {
+    console.log('Fetch API not found, try including the polyfill');
+    return;
+  }
 
   function fetchJSON() {
-    // TODO 2.1b
+    fetch('examples/animals.json') // 1
+    .then(validateResponse) // 2
+    .then(readResponseAsJSON) // 3
+    .then(logResult) // 4
+    .catch(logError);
   }
 
   function validateResponse(response) {
-    // TODO 2.3
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
   }
 
   function readResponseAsJSON(response) {
-    // TODO 2.4
+    return response.json();
   }
 
   function showImage(responseAsBlob) {
-    //  TODO 3a
+    var container = document.getElementById('container');
+    var imgElem = document.createElement('img');
+    container.appendChild(imgElem);
+    var imgUrl = URL.createObjectURL(responseAsBlob);
+    imgElem.src = imgUrl;
   }
 
   function readResponseAsBlob(response) {
-    // TODO 3b
+    return response.blob();
   }
 
   function fetchImage() {
-    // TODO 3c
+    fetch('examples/kitten.jpg')
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError);
   }
 
   function showText(responseAsText) {
-    //  TODO 4a
+    var message = document.getElementById('message');
+    message.textContent = responseAsText;
   }
 
   function readResponseAsText(response) {
-    // TODO 4b
+    return response.text();
   }
 
   function fetchText() {
-    // TODO 4c
+    fetch('examples/words.txt')
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    .catch(logError);
   }
 
   function headRequest() {
-    // TODO 5.1
+    fetch('examples/words.txt', {
+      method: 'HEAD'
+    })
+    .then(validateResponse)
+    .then(logSize)
+    .then(readResponseAsText)
+    .then(logResult)
+    .catch(logError);
   }
 
   function logSize(response) {
-    // TODO 5.2
+    console.log(response.headers.get('content-length'));
+    return response;
   }
 
   /* NOTE: Never send unencrypted user credentials in production! */
   function postRequest() {
-    // TODO 6.2
+    var formData = new FormData(document.getElementById('myForm'));
+    fetch('http://localhost:5000/', {
+      method: 'POST',
+      body: formData,
+      mode: 'cors', // This is optional - mode's default value is 'cors'
+      headers: customHeaders
+    })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(logResult)
+    .catch(logError);
   }
+
+  var customHeaders = new Headers({
+    'Content-Type': 'text/plain',
+    // 'Content-Length': 'kittens' // Content-Length can't be modified!
+    'X-Custom': 'hello world',
+    // 'Y-Custom': 'this won\'t work' // Y-Custom is not accepted by our echo server!
+  });
 
   // Don't worry if you don't understand this, it's not part of the Fetch API.
   // We are using the JavaScript Module Pattern to enable unit testing of
